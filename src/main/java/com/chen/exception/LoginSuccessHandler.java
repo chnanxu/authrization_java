63234aa09107648b30a8f88d1551383c17139649
TomoriNao63 <455227671@qq.com>
2023-09-21 18:12:25 +0800

    修改

--- src/main/java/com/chen/exception/LoginSuccessHandler.java
+++ src/main/java/com/chen/exception/LoginSuccessHandler.java
@@ -1,7 +1,10 @@
 package com.chen.exception;
 
 import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
+import com.chen.utils.result.Code;
 import com.chen.utils.result.CommonCode;
+import com.chen.utils.result.JwtUtils;
 import com.chen.utils.result.ResponseResult;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,14 +15,20 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setContentType("application/json,charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
-        ResponseResult result=new ResponseResult(CommonCode.SUCCESS,authentication);
-        PrintWriter writer=response.getWriter();
-        writer.write(JSONUtil.toJsonStr(result));
+        System.out.println(authentication);
+        String token = JwtUtils.token(authentication);
+        response.addHeader("token",token);
+        Map<String,Object> tokenInfo=new HashMap<>();
+        tokenInfo.put("token",token);
+        ResponseResult result=new ResponseResult(CommonCode.SUCCESS,tokenInfo);
+        response.getWriter().write(JSON.toJSONString(result));
     }
 }
